1b697dd5be156cb148499bfdc9a8474b95e55695
Linden Kwok <lindenstkwok@gmail.com>
2021-01-29 16:27:16 -0800

    File Structure Organization

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static('./views'));
+app.use(express.static('./public'));
 
 app.get("/", (req, res) => {
-    res.sendFile('views/index.html', {root: __dirname});
+    res.sendFile('index.html', {root: __dirname});
     res.end();
 })
 
