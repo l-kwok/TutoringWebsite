610c9fb778d12a0bfad9d8b938cadf10ce907c4b
Linden Kwok <lindenstkwok@gmail.com>
2021-01-30 19:38:00 -0800

    Some Mobile Optimzation Support
    Fixed some scaling issues for scaling to mobile phones and tablets. Disabled Parallaxing Pages

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,38 +5,38 @@ function parallax(element, distance, speed){
 }
 
 window.addEventListener('scroll', function(){
-    parallax('header', window.scrollY, 1);
-    parallax('#bg', window.scrollY, 1);
+    // parallax('header', window.scrollY, 1);
+    // parallax('#bg', window.scrollY, 1);
 
 
     let scrollAmount = document.querySelector('header').offsetHeight;
-    document.querySelector('#about').style.fill = `white`;
+    // document.querySelector('#about').style.fill = `white`;
 
     //delay for fade in text for about
     if(window.scrollY > scrollAmount - 350){
         $("#about div").addClass("fadeAnim");
     }
 
-    if(window.scrollY > scrollAmount){
-        parallax('#about', window.scrollY - document.querySelector('header').offsetHeight, 1);
-        scrollAmount += document.querySelector('header').offsetHeight;
-    }
+    // if(window.scrollY > scrollAmount){
+    //     parallax('#about', window.scrollY - document.querySelector('header').offsetHeight, 1);
+    //     scrollAmount += document.querySelector('header').offsetHeight;
+    // }
 
-    if(window.scrollY > scrollAmount){
-        parallax('#subjects', window.scrollY - scrollAmount, 1);
-        scrollAmount += document.querySelector('#about').offsetHeight;
-    }
+    // if(window.scrollY > scrollAmount){
+    //     parallax('#subjects', window.scrollY - scrollAmount, 1);
+    //     scrollAmount += document.querySelector('#about').offsetHeight;
+    // }
 
-    if(window.scrollY >= scrollAmount){
-        parallax('#contact', window.scrollY - scrollAmount, 1);
-        scrollAmount += document.querySelector('footer').offsetHeight;
-    }
+    // if(window.scrollY >= scrollAmount){
+    //     parallax('#contact', window.scrollY - scrollAmount, 1);
+    //     scrollAmount += document.querySelector('footer').offsetHeight;
+    // }
 
-    if(window.scrollY >= scrollAmount){
-        parallax('footer', window.scrollY - scrollAmount, 1);
-        // window.scrollY = 0;
-    }
-    console.log(scrollAmount);
+    // if(window.scrollY >= scrollAmount){
+    //     parallax('footer', window.scrollY - scrollAmount, 1);
+    //     // window.scrollY = 0;
+    // }
+    // console.log(scrollAmount);
 
 
     
